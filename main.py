59a5852dcf9cd22cb6d651610fb1042b500e902e
qwe <aleksey18003@yandex.ru>
2024-09-04 20:41:16 +0300

    update

--- main.py
+++ main.py
@@ -9,16 +9,9 @@ from models.models import Word, TranslationWord, session_maker
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
