2b3d95e3120a4a970487b73e819edd093b673cce
Aleksey <aleksey18003@yandex.ru>
2024-09-10 13:29:49 +0300

    update

--- models/models.py
+++ models/models.py
@@ -1,8 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 
-engine = create_async_engine("postgresql+asyncpg://postgres:postgres@db:5432/postgres", echo=True)
+engine = create_async_engine("postgresql+asyncpg://qwe:qwe@localhost/fastapi", echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
@@ -14,8 +16,14 @@ class Base(DeclarativeBase):
 class Word(Base):
     __tablename__ = 'words'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
     name: Mapped[str]
+    part_of_speech: Mapped[str]
+    rating: Mapped[str]
     translation_id: Mapped[int] = mapped_column(ForeignKey('translation_words.id'))
 
     translation: Mapped["TranslationWord"] = relationship(back_populates="words")
@@ -24,7 +32,11 @@ class Word(Base):
 class TranslationWord(Base):
     __tablename__ = 'translation_words'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
     name: Mapped[str]
 
     words: Mapped["Word"] = relationship(back_populates="translation")
