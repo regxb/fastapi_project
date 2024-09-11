9f492417c7d5f9dac0139b8fa754bd41f01315d4
Aleksey <aleksey18003@yandex.ru>
2024-09-11 13:42:50 +0300

    another folder

---
+++ src/main.py
@@ -0,0 +1,79 @@
+import random
+import uuid
+
+from fastapi import FastAPI, Depends
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
+from fastapi.middleware.cors import CORSMiddleware
+
+from .models import Word
+from .database import get_async_session
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+part_of_speech = [
+    'noun', 'verb', 'adjective', 'adverb', 'determiner',
+    'pronoun', 'preposition', 'numeral', 'conjunction', 'other'
+]
+
+
+@app.get("/get-word/eng")
+async def get_word(session: AsyncSession = Depends(get_async_session)):
+    random_part_of_speech = random.choice(part_of_speech)
+    query = select(Word).where(Word.part_of_speech == random_part_of_speech, Word.rating == "A1"
+                               ).options(joinedload(Word.translation)).order_by(func.random()).limit(3)
+    result = await session.execute(query)
+    random_words = [random_word for random_word in result.scalars().all()]
+    word_for_translate = random_words[0]
+    random.shuffle(random_words)
+
+    return {
+        "word_for_translate": {'id': word_for_translate.id,
+                               'name': word_for_translate.name},
+        "other_words": [
+            {'id': word.translation.id, 'name': word.translation.name} for word in random_words
+        ]
+    }
+
+
+@app.get("/get-word/rus")
+async def get_word(session: AsyncSession = Depends(get_async_session)):
+    random_part_of_speech = random.choice(part_of_speech)
+    query = select(Word).where(Word.part_of_speech == random_part_of_speech, Word.rating == "A1"
+                               ).options(joinedload(Word.translation)).order_by(func.random()).limit(3)
+    result = await session.execute(query)
+    random_words = [random_word for random_word in result.scalars().all()]
+    word_for_translate = random_words[0]
+    random.shuffle(random_words)
+
+    return {
+        "word_for_translate": {'id': word_for_translate.id,
+                               'name': word_for_translate.translation.name},
+        "other_words": [
+            {'id': word.id, 'name': word.name} for word in random_words
+        ]
+    }
+#
+#
+# @app.get("/check_answer")
+# async def check_answer(word_for_translate_id: uuid.UUID, user_choice_word_id: uuid.UUID):
+#     async with async_sessionmaker() as session:
+#         query = select(Word).where(Word.id == word_for_translate_id)
+#         word_for_translate = await session.scalar(query)
+#     if word_for_translate is None:
+#         return f"Слово с id {word_for_translate_id} не найдено"
+#     if word_for_translate.translation_id == user_choice_word_id:
+#         return True
+#     return False
+
+# @app.post("/exam")
+# async def exam(user_id: int):
