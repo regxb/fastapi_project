35b3385445b029e6bc69b0e02d558597e5371fea
Aleksey <aleksey18003@yandex.ru>
2024-09-15 19:54:39 +0300

    add a function to check favorite words

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,16 @@
 import random
+from uuid import uuid4
 
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from .constants import part_of_speech_list
-from .models import Word
+from .models import Word, FavoriteWord
 
 
 async def get_random_words(session: AsyncSession):
-    random_part_of_speech = random.choice(part_of_speech_list)
+    random_part_of_speech = random.choice(list(part_of_speech_list.keys()))
     query = select(Word).where(Word.part_of_speech == random_part_of_speech, Word.rating == "A1"
                                ).options(joinedload(Word.translation)).order_by(func.random()).limit(3)
     result = await session.execute(query)
@@ -17,3 +18,12 @@ async def get_random_words(session: AsyncSession):
     word_for_translate = random_words[0]
     random.shuffle(random_words)
     return word_for_translate, random_words
+
+
+async def check_favorite_words(user_id: int, word_id: uuid4, session: AsyncSession):
+    query = select(FavoriteWord).where(and_(FavoriteWord.user_id == user_id, FavoriteWord.word_id == word_id))
+    result = await session.scalar(query)
+    if result:
+        return True
+    else:
+        return False
