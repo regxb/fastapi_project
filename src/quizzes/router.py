822d1682c0d358b10d22f6be0b368ba9cfe5fa90
Aleksey <aleksey18003@yandex.ru>
2024-09-15 15:26:20 +0300

    can add word to  favorites

--- src/quizzes/router.py
+++ src/quizzes/router.py
@@ -2,13 +2,13 @@ import uuid
 from typing import Optional, List
 
 from fastapi import Depends, HTTPException, APIRouter
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from src.constants import part_of_speech_list
-from src.models import Word
-from src.quizzes.schemas import AnswerResponse
+from src.models import Word, FavoriteWord, User
+from src.quizzes.schemas import AnswerResponse, FavoriteWordBase
 from src.schemas import WordInfo
 from src.utils import get_random_words
 from src.database import get_async_session
@@ -78,3 +78,28 @@ async def get_word_from_part_of_speech(part_of_speech: str, session: AsyncSessio
     )
 
     return response_data
+
+
+@router.post("/favorite-word")
+async def add_favorite_word(data: FavoriteWordBase, session: AsyncSession = Depends(get_async_session)):
+    query = select(User).where(User.telegram_id == data.telegram_id)
+    result = await session.scalar(query)
+    if result:
+        user_id = result.id
+    else:
+        raise HTTPException(status_code=404, detail="Пользователь не найден")
+    word = await session.scalar(select(Word).where(Word.id == data.word_id))
+    if word is None:
+        raise HTTPException(status_code=404, detail="Слово не найдено")
+    query = (select(FavoriteWord).where(and_(FavoriteWord.word_id == data.word_id, FavoriteWord.user_id == user_id)))
+    result = await session.execute(query)
+    user_favorite_words = result.scalars().all()
+    if user_favorite_words:
+        raise HTTPException(status_code=201, detail="Данное слово уже добавлено пользователем")
+    new_favorite_word = FavoriteWord(
+        user_id=user_id,
+        word_id=data.word_id
+    )
+    session.add(new_favorite_word)
+    await session.commit()
+    return {"message": "Слово успешно добавлено"}
--- src/quizzes/schemas.py
+++ src/quizzes/schemas.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 
 from src.schemas import WordInfo
 
@@ -8,3 +8,8 @@ from src.schemas import WordInfo
 class AnswerResponse(BaseModel):
     word_for_translate: WordInfo
     other_words: List[WordInfo]
+
+
+class FavoriteWordBase(BaseModel):
+    telegram_id: int
+    word_id: UUID4
