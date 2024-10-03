88b27e9a0ef90ecf75b1f6963960ee220b7c4e59
Aleksey <aleksey18003@yandex.ru>
2024-10-03 17:14:32 +0300

    added a separate function to get a random word from the list of favorite words

--- src/quizzes/query.py
+++ src/quizzes/query.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, func, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from src.models import Word, TranslationWord
+from src.models import Word, TranslationWord, FavoriteWord
 
 
 async def get_random_word_for_translate(session: AsyncSession, language_from_id: int):
@@ -21,3 +21,13 @@ async def get_random_words(session: AsyncSession, language_to_id: int, word_for_
     result = await session.execute(query)
     other_words = [w for w in result.scalars().all()]
     return other_words
+
+
+async def get_random_user_favorite_word(session: AsyncSession, user_id: int):
+    query = ((select(FavoriteWord).join(FavoriteWord.word).join(Word.translation)
+             .options(joinedload(FavoriteWord.word).options(joinedload(Word.translation)))
+             .where(FavoriteWord.user_id == user_id))
+             .order_by(func.random())
+             .limit(1))
+    random_user_favorite_word = await session.scalar(query)
+    return random_user_favorite_word
