1ed963e8cc622b59275193ee4e251ec06c0715e5
Aleksey <aleksey18003@yandex.ru>
2024-10-03 12:14:28 +0300

    add response schemas for get_random_word

--- src/quizzes/schemas.py
+++ src/quizzes/schemas.py
@@ -5,27 +5,32 @@ from pydantic import BaseModel, UUID4
 from src.schemas import WordInfo
 
 
-class AnswerResponse(BaseModel):
+# class AnswerResponse(BaseModel):
+#     word_for_translate: WordInfo
+#     other_words: List[WordInfo]
+#     in_favorites: bool
+#
+#
+# class SentenceInfo(BaseModel):
+#     name: str
+#     id: UUID4
+#
+#
+# class SentenceAnswerResponse(BaseModel):
+#     sentence_for_translate: SentenceInfo
+#     other_words: List[str]
+#
+#
+# class FavoriteAnswerResponse(BaseModel):
+#     word_for_translate: WordInfo
+#     other_words: List[WordInfo]
+#
+#
+# class FavoriteWordBase(BaseModel):
+#     telegram_id: int
+#     word_id: UUID4
+
+
+class RandomWordResponse(BaseModel):
     word_for_translate: WordInfo
     other_words: List[WordInfo]
-    in_favorites: bool
-
-
-class SentenceInfo(BaseModel):
-    name: str
-    id: UUID4
-
-
-class SentenceAnswerResponse(BaseModel):
-    sentence_for_translate: SentenceInfo
-    other_words: List[str]
-
-
-class FavoriteAnswerResponse(BaseModel):
-    word_for_translate: WordInfo
-    other_words: List[WordInfo]
-
-
-class FavoriteWordBase(BaseModel):
-    telegram_id: int
-    word_id: UUID4
