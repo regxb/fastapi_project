55d53771b710da74d284a9c85f082727b2923a90
Aleksey <aleksey18003@yandex.ru>
2024-10-15 22:10:11 +0300

    add update_user_progress to exam utils.py

--- src/exams/utils.py
+++ src/exams/utils.py
@@ -1,3 +1,9 @@
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models import Exam, User
+
+
 async def update_user_rating(user_rating: str) -> str:
     if user_rating == "A1":
         return "A2"
@@ -11,3 +17,24 @@ async def update_user_rating(user_rating: str) -> str:
         return "C2"
     elif user_rating == "C2":
         return "C2"
+
+
+async def update_user_progress(result: bool, user_exam: Exam, user: User, session: AsyncSession):
+    if result:
+        if user_exam.progress == user_exam.total_exercises:
+            user_exam.status = "completed"
+            new_user_rating = await update_user_rating(user.rating)
+            user.rating = new_user_rating
+            await session.commit()
+            return {"message": "exam is completed"}
+        user_exam.progress += 1
+        await session.commit()
+        return True
+    else:
+        if user_exam.attempts == 0:
+            user_exam.status = "failed"
+            await session.commit()
+            return {"message": "Exam is failed"}
+        user_exam.attempts -= 1
+        await session.commit()
+        return False
