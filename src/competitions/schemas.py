378b9bc27c1973dae2127ead2507a8614bd3358d
Aleksey <aleksey18003@yandex.ru>
2024-12-02 22:22:51 +0300

    add new schemas for competitions

--- src/competitions/schemas.py
+++ src/competitions/schemas.py
@@ -1,4 +1,5 @@
 import uuid
+from typing import List
 
 from pydantic import BaseModel
 
@@ -13,3 +14,26 @@ class CompetitionAnswerSchema(BaseModel):
     user_word_id: uuid.UUID
     telegram_id: int
     room_id: int
+
+
+class CompetitionsSchema(BaseModel):
+    telegram_id: int
+    language_from_id: int
+    language_to_id: int
+
+
+class AnsweredUsersSchema(BaseModel):
+    telegram_id: int
+    success: bool
+
+
+class UserStatsSchema(BaseModel):
+    telegram_id: int
+    points: int
+
+
+class CompetitionsAnswersSchema(BaseModel):
+    answered_user: AnsweredUsersSchema
+    selected_word_id: uuid.UUID
+    correct_word_id: uuid.UUID
+    users: List[UserStatsSchema]
