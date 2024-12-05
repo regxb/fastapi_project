fed004d9c40fd007f719f489f625eee5e8040041
Aleksey <aleksey18003@yandex.ru>
2024-12-06 00:19:33 +0300

    add new dependencies - redis

--- src/competitions/dependencies.py
+++ src/competitions/dependencies.py
@@ -1,12 +1,22 @@
 from src.competitions.service import RoomManager, WebSocketManager
+import redis.asyncio as redis
 
-room_manager = RoomManager()
 websocket_manager = WebSocketManager()
-
-
-def get_room_manager() -> RoomManager:
-    return room_manager
 
 
 def get_websocket_manager() -> WebSocketManager:
     return websocket_manager
+
+
+redis_pool = redis.ConnectionPool.from_url("redis://redis:6379", max_connections=10)
+
+
+def get_redis() -> redis.Redis:
+    return redis.Redis(connection_pool=redis_pool)
+
+
+room_manager = RoomManager(get_redis())
+
+
+def get_room_manager() -> RoomManager:
+    return room_manager
