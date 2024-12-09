6c0beda7665252088c0dcb45174e7cc3bcf56ebf
Aleksey <aleksey18003@yandex.ru>
2024-12-09 04:06:29 +0300

    add depends for telegram bot

--- src/competitions/dependencies.py
+++ src/competitions/dependencies.py
@@ -1,9 +1,18 @@
+from aiogram import Bot
+
 from src.competitions.service import RoomManager, WebSocketManager
 from src.database import get_redis
 from src.words.service import CacheRedisService
+from src.config import BOT_TOKEN
+
 
 websocket_manager = WebSocketManager()
 room_manager = RoomManager(get_redis())
+bot = Bot(token=BOT_TOKEN)
+
+
+def get_tg_bot() -> Bot:
+    return bot
 
 
 def get_websocket_manager() -> WebSocketManager:
